1b449307bc25685c562c350cacff2c843c61dfba
Helmeppun <52864839+Helmeppun@users.noreply.github.com>
2023-03-28 13:19:11 -0300

    Ajuste no corpo do código para correção de erros.

--- client.py
+++ client.py
@@ -2,10 +2,10 @@ from paho.mqtt import client as mqtt_client
 from random import randint
 from time import sleep
 
-class client:
+
+class Client:
     """
     Fornece funcionalidades de conexão e comunicação MQTT
-
         Argumentos:
             broker (str): endereço do broker
             port (int): porta de conexão do broker
@@ -14,6 +14,7 @@ class client:
             username (str): nome de usuário
             password (str): senha para conexão
     """
+
     def __init__(self):
         self.broker = 'broker.emqx.io'
         self.port = 1883
@@ -21,11 +22,10 @@ class client:
         self.client_id = f'mqtt-{randint(0, 1000)}'
         self.username = 'emqx'
         self.password = 'public'
-    
+
     def on_connect(self, client, userdata, flags, rc):
         """
         Retorna o status da conexão (callback) de acordo com a resposta do servidor
-
             Argumentos:
                 client (): cliente MQTT
                 userdata ():
@@ -46,36 +46,34 @@ class client:
         # Define id do cliente conectado
         client = mqtt_client.Client()
         client.username_pw_set(self.username, self.password)
-        client.on_connect = self.on_connect()
+        client.on_connect = self.on_connect
         client.connect(self.broker, self.port)
         return client
-    
+
     def on_message(self, client, userdata, message):
         """
         Exibe as mensagens exibidas dos tópicos
-
             Argumentos:
                 client (): cliente MQTT
                 userdata ():
                 message (str): mensagem recebida
         """
         print(f"Mensagem `{message.payload.decode()}` recebido do tópico `{message.topic}`")
-    
+
     def subscribe(self, client: mqtt_client, topic):
         """
         Increve os cliente nos tópicos do broker
-
             Argumentos:
                 client (): cliente MQTT
                 topic (str): tópico do broker
         """
         client.subscribe(topic)
-        client.on_message = self.on_message()
+        client.on_message = self.on_message
 
     def publish(self, client, topic, message):
         """
         Publica mensagens nos tópicos do broker
-            
+
             Argumentos:
                 client (): cliente MQTT
                 topic (str): tópico do broker
@@ -90,12 +88,13 @@ class client:
                 print(f"Enviando `{message}` para o tópico `{topic}`")
             else:
                 print(f"Falha ao enviar mensagem para o tópico {topic}")
-    
+
     def main(self):
         client = self.connect_mqtt()
 
         client.loop_forever()
         self.publish(client, self.topic1, "oi")
 
-client_inst = client()
-client.main()
+
+client_inst = Client()
+client_inst.main()
