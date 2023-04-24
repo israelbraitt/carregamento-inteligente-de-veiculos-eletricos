5ea314882b382d977a3f2d7ec5fa000bab967ad7
Helmeppun <52864839+Helmeppun@users.noreply.github.com>
2023-04-23 21:11:46 -0300

    metódos pra obter melhor caminho e atualizar base de dados

--- central server/central_server.py
+++ central server/central_server.py
@@ -5,28 +5,27 @@ import json
 from station import Station
 import DAO
 
+
 class CentralServer:
     """
     Servidor que processa as requisições dos servidores locais
-
         Atributos:
             cloud_host (str): endereço de conexão do socket TCP
             cloud_port (int): porta de conexão do socket TCP
             cloud_socket (socket): inicialização do socket TCP para comunicação com o servidor central
-
             station_list (list): lista de postos de carregamento associados ao serviço
-
             format (str): formato da codificação de caracteres
     """
+
     def __init__(self):
         """
         Método construtor da classe
         """
-        self.cloud_host = '127.0.0.1'
-        self.cloud_port = 1917
+        self.CLOUD_HOST = socket.gethostbyname(socket.gethostname())
+        self.CLOUD_PORT = 1917
         self.socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        self.station_list = []
+
+        self.station_dict = {}
 
         self.format = 'utf-8'
 
@@ -40,7 +39,8 @@ class CentralServer:
         try:
             # Fornece o endereço e as portas para "escutar" as conexões
             # com os sockets dos clientes
-            self.socket_tcp.bind((self.tcp_host, self.tcp_port))
+            print(self.CLOUD_HOST)
+            self.socket_tcp.bind((self.CLOUD_HOST, self.CLOUD_PORT))
             self.socket_tcp.listen()
         except:
             return print("Não foi possível iniciar o sistema de informações")
@@ -49,46 +49,77 @@ class CentralServer:
             # Aceita a conexão com os sockets dos clientes
             conn_client_tcp, addr_client_tcp = self.socket_tcp.accept()
             print("Conectado com um cliente TCP em: ", addr_client_tcp)
-            new_station = Station()
 
             # Recebe mensagens dos clientes através da conexão TCP
-            thread_tcp = threading.Thread(target=self.tratarRequests, args=[conn_client_tcp])
+            thread_tcp = threading.Thread(target=self.tratarServer, args=(conn_client_tcp, addr_client_tcp))
             thread_tcp.start()
 
-    def recvMessage(self):
-        """
-        Recebe mensagens via TCP dos servidores locais
-        """
-        return self.socket_tcp.recv(1024)
-    
-    def loadStationList (self):
-        """
-        Carrega a lista de postos de carregamento cadastrados
-        """
-        self.station_list = DAO.getStationList()
+    def tratarServer(self, client, addr):
+        connected = True
+        print(f"\n===CONEXÃO COM {addr} ESTABELECIDA.===\n")
+        try:
+            while connected:
+                msg = client.recv(1024).decode(self.format)
+                msg = str(msg)
+                msg = json.loads(msg)
+                print("===MENSAGEM RECEBIDA===")
+                print(msg)
+                response = None
+
+                if msg.get("time_left"):
+                    response = self.chooseBestStation(msg)
+                elif msg.get("queue"):
+                    response = self.updateStation(msg)
+
+                if response:
+                    print(f"\n===ENVIANDO RESPOSTA:=== \n{response}")
+                    client.send(response.encode(self.format))
+
+        except Exception as e:
+            print(f"\n===OCORREU UM ERRO NA COMUNICAÇÃO COM {addr}.===")
+            print(e)
+
+    def updateStation(self, station_info):
+        print(self.station_dict)
+
+        new_location = station_info.get("location")
+        new_code = station_info.get("code")
+        new_queue = station_info.get("queue")
+        new_station = Station(new_location, new_code, new_queue)
+        self.station_dict[new_code] = new_station
+        print(self.station_dict)
+
+        response = "{\"result\": \"1\"}"
+        return response
 
     def chooseBestStation(self, car_info):
         """
         Escolhe o melhor posto entre as opções disponíveis para o carro recarregar
-        
+
             Argumentos:
                 car_info (): informações do carro (bateria e modo de autonomia)
         """
+
         best_queue = 25
         best_station = None
-        
-        for station in self.station_list:
-            time_to_station = 0
-            if car_info["time left"] ==  time_to_station:
-                if station.queue < best_queue:
-                    best_station = station
-        
+
+        for station in self.station_dict.values():
+            if station.location != car_info.get("location"):
+                current_distance = station.distance(int(car_info.get("location")))
+                current_time = current_distance * 10
+                if int(car_info.get("time_left")) > current_time:
+                    if int(station.queue) < best_queue:
+                        best_queue = station.queue
+                        best_station = station
         if best_station:
-            return best_station
+            return best_station.getJson()
         else:
-            pass
+            return "{\"result\": \"posto não encontrado\"}"
+
 
     def main(self):
         # Cria um socket com conexão TCP
-        tcp_thread = threading.Thread(target=self.conexaoTCP, args=[])
-        tcp_thread.start()
+        self.conexaoTCP()
+
+central = CentralServer()
+central.main()
